0d6bcca063b08e1b02715ad1a3279022193f2c55
AbdelouahedSN <abdosenane75@gmail.com>
2023-10-25 16:00:57 +0100

    Added new features

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,7 @@ btnAccordian.forEach((e) => {
 });
 
 // ============ Accordian  ===============
+
 // ============ Validation form  ============
 
 const btnSubmit = document.getElementById("submitBtn");
